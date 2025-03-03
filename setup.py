21ef25597e5dbba4fb76b879ecf03fab1637aa05
Evermacea <144848387+Evermacea@users.noreply.github.com>
2025-03-03 00:33:24 +0000

    error de libreria "requests" corregido

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     install_requires=[
         "pandas",
         "matplotlib",
-        "plotly"  # Aquí debe ser "plotly", NO "plotly.express"
+        "plotly",  # Aquí debe ser "plotly", NO "plotly.express"
+        "requests"
     ],
 )
